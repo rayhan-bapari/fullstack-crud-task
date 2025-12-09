7f44a7bb60c4e6894515a847b541e8ecd0550af3
rayhan-bapari <mdrayhanbapari02@gmail.com>
2025-12-09 12:20:46 +0600

    setup vue

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel + Vue</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
